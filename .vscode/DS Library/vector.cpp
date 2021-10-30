4d05614174dd7e6eb4ff67c0cdaada756d46e136
tejassathe117 <53342750+tejassathe117@users.noreply.github.com>
2021-10-30 23:00:10 +0530

    Add front and end funtion toget element

--- .vscode/DS Library/vector.cpp
+++ .vscode/DS Library/vector.cpp
@@ -64,6 +64,18 @@ public:
 
 	void pop_back() { currentSize--; }
 
+    int front() 
+    { 
+	  if(currentSize == 0) { return 0; }
+	  return arr[0];
+	}
+	
+	int end()
+    {
+	  if(currentSize == 0) { return 0; }
+	  return arr[currentSize-1];
+	}
+
 	int size() { return currentSize; }
 
 	int getcapacity() { return capacity; }
@@ -81,16 +93,11 @@ int main()
 {
   int nums[] = {1, 2, 3};
 	Vector<int> v(nums, 3);
-	Vector<char> v1;
 	v.push_back(10);
 	v.push_back(20);
 	v.push_back(30);
 	v.push_back(40);
 	v.push_back(50);
-	v1.push_back(71);
-	v1.push_back(72);
-	v1.push_back(73);
-	v1.push_back(74);
 
 	cout << "Vector size : " << v.size() << endl;
 	cout << "Vector capacity : " << v.getcapacity() << endl;
@@ -102,32 +109,27 @@ int main()
 
 	cout << "\nAfter updating 1st index" << endl;
 
-	cout << "Vector elements of type int : " << endl;
+	cout << "Vector elements: " << endl;
 	v.print();
-	cout << "Vector elements of type char : " << endl;
-	v1.print();
-	cout << "Element at 1st index of type int: " << v.get(1)
-		<< endl;
-	cout << "Element at 1st index of type char: "
-		<< v1.get(1) << endl;
+	
+	cout << "Element at 1st index: " << v.get(1) << endl;
+
 
 	v.pop_back();
-	v1.pop_back();
-
+	
 	cout << "\nAfter deleting last element" << endl;
 
-	cout << "Vector size of type int: " << v.size() << endl;
-	cout << "Vector size of type char: " << v1.size()
-		<< endl;
-	cout << "Vector capacity of type int : "
-		<< v.getcapacity() << endl;
-	cout << "Vector capacity of type char : "
-		<< v1.getcapacity() << endl;
+	cout << "Vector size: " << v.size() << endl;
+	
+	cout << "Vector capacity: " << v.getcapacity() << endl;
 
-	cout << "Vector elements of type int: ";
+
+	cout << "Vector elements: ";
 	v.print();
-	cout << "Vector elements of type char: ";
-	v1.print();
+	
+	cout << "Front Vector element: " << v.front();
+	cout << endl;
+    cout << "Back Vector element: " << v.end(); 
 
-	return 0;
+	return 0;   
 }
