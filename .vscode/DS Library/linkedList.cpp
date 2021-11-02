16dd024f74a9abdb3ccdc84bec8a8ba67be07340
tejassathe117 <53342750+tejassathe117@users.noreply.github.com>
2021-11-02 23:14:58 +0530

    Add Prepend Function

--- .vscode/DS Library/linkedList.cpp
+++ .vscode/DS Library/linkedList.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 
 class Node{
     public:
@@ -11,15 +12,45 @@ class Node{
 };
 
 class LinkedList{
+    public:
     Node* head = NULL;
 
     Node* createNewNode(int data){
         Node* newNode = new Node();
         newNode->data = data;
         newNode->next = NULL;
+        return newNode;
     }
+
+    void insertInTheBeginning(int data){
+        Node* temp = createNewNode(data);
+        if(head == NULL){
+            head = temp;
+        } else {
+            temp -> next = head;
+            head = temp;
+            cout << "Node Prepended" << endl;
+        }
+    }
+
+    void printLinkedList(){
+        Node* temp = head;
+        while(temp->next != NULL){
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+
 };
 
 int main(){
+    LinkedList list;
+    list.insertInTheBeginning(5);
+    list.insertInTheBeginning(6);
+    list.insertInTheBeginning(7);
+    list.insertInTheBeginning(8);
+    list.insertInTheBeginning(9);
 
+    list.printLinkedList();
 }
